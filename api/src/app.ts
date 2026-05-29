feedb3fc2bc704ddd7cb17383ddfcdfea57db246
PC-Ngumoha <pcngumoha@gmail.com>
2026-05-29 13:50:31 +0100

    CORS connection to frontend

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // Access-Control-Allow-Origin: *
+
+app.get('/api/v1/test', (req: Request, res: Response) => {
+  res.json({"message": "Hello from the backend"});
+})
 
 export default app;
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Timeline from "./features/Timeline/Timeline";
 import Auth from "./features/Auth/Auth";
 
 function App() {
+  useEffect(() => {
+    async function testing() {
+      const response = await fetch('http://localhost:3000/api/v1/test');
+      const data = await response.json();
+      console.log(data);
+    }
+
+    testing();
+  }, []);
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Timeline />}/>
