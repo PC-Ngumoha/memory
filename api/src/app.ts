d4452cc2ebe1d13056233654bc7a7876a37add4d
PC-Ngumoha <pcngumoha@gmail.com>
2026-06-01 17:47:13 +0100

    Incorporate Auth service: auth0

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,23 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { auth } from 'express-oauth2-jwt-bearer';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors()); // Access-Control-Allow-Origin: *
 
-app.get('/api/v1/test', (req: Request, res: Response) => {
-  res.json({"message": "Hello from the backend"});
+const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`,
+  tokenSigningAlg: 'RS256',
+})
+
+app.get('/api/v1/test', checkJwt, (req: Request, res: Response) => {
+  res.json({"message": "Hello from the backend", user: req.auth });
 })
 
 export default app;
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,92 @@
-import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import Timeline from "./features/Timeline/Timeline";
-import Auth from "./features/Auth/Auth";
+// import { useEffect } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router";
+// import Timeline from "./features/Timeline/Timeline";
+// import Auth from "./features/Auth/Auth";
+
+import { useAuth0 } from '@auth0/auth0-react';
+
+// function App() {
+//   useEffect(() => {
+//     async function testing() {
+//       const response = await fetch('http://localhost:3000/api/v1/test');
+//       const data = await response.json();
+//       console.log(data);
+//     }
+
+//     testing();
+//   }, []);
+//   return <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Timeline />}/>
+//       <Route path="/auth" element={<Auth />}/>
+//     </Routes>
+//   </BrowserRouter>;
+// }
 
 function App() {
-  useEffect(() => {
-    async function testing() {
-      const response = await fetch('http://localhost:3000/api/v1/test');
-      const data = await response.json();
-      console.log(data);
-    }
+  const {
+    getAccessTokenSilently,
+    user,
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect: login,
+    logout: auth0Logout,
+    error,
+  } = useAuth0();
 
-    testing();
-  }, []);
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Timeline />}/>
-      <Route path="/auth" element={<Auth />}/>
-    </Routes>
-  </BrowserRouter>;
+  const signup = () =>
+    login({ authorizationParams: { screen_hint: 'signup' } });
+
+  const logout = () =>
+    auth0Logout({ logoutParams: { returnTo: window.location.origin } });
+
+  const callApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      const res = await fetch(
+        `${import.meta.env.VITE_AUTH0_AUDIENCE}/api/v1/test`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (isLoading) return 'Loading...';
+
+  return isAuthenticated ? (
+    <>
+      <p>Logged in as {user!.email}</p>
+
+      <h1>User Profile</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <button onClick={callApi}>Call Protected API route</button>
+      <button onClick={logout}>Logout</button>
+    </>
+  ) : (
+    <>
+      {error && <p>Error: {error.message}</p>}
+
+      <button onClick={signup}>Signup</button>
+
+      <button
+        onClick={() => {
+          login();
+        }}
+      >
+        Login
+      </button>
+    </>
+  );
 }
 
 export default App;
